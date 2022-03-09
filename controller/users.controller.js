53bc0a0e2887b7e499602330de0364543dff4120
FirasKhadraoui <firas.khadraoui@esprit.tn>
2022-03-09 14:00:22 +0100

    g-FrontReact

--- controller/users.controller.js
+++ controller/users.controller.js
@@ -1,55 +1,76 @@
-const Users = require ('../models/users.models')
-const ValidateUser = require ('../validation/users.validation')
+const Users = require('../models/users.models')
+const ValidateUser = require('../validation/users.validation')
 
-const addUser =  async (req,res)=>{
+
+const addUser = async (req, res) => {
+    const { errors, isValid } = ValidateUser(req.body)
     try {
-        await Users.create(req.body)
-        res.status(201).json({message:'User add with success'})
+        if (!isValid) {
+            res.status(404).json(errors);
+        }
+        else {
+            await Users.findOne({ Email: req.body.Email }).then(async (exist) => {
+                if (exist) {
+                    errors.Email = "User Exist";
+                    res.status(404).json(errors);
+                }
+                else {
+                    await Users.create(req.body)
+                    res.status(201).json({ message: 'User add with success' });
+                }
+            });
+        }
     } catch (error) {
         console.log(error.message)
     }
-}
+};
 
-const findAllUsers =  async (req,res)=>{
+const findAllUsers = async (req, res) => {
     try {
         const data = await Users.find()
-        res.status(201).json({data})
+        res.status(201).json({ data })
     } catch (error) {
         console.log(error.message)
     }
 }
 
-const findUser =  async (req,res)=>{
+const findUser = async (req, res) => {
     try {
-        const data = await Users.findOne({_id:req.params.id})
+        const data = await Users.findOne({ _id: req.params.id })
         res.status(201).json(data)
     } catch (error) {
         console.log(error.message)
     }
 }
 
-const updateUser =  async (req,res)=>{
+const updateUser = async (req, res) => {
+    const { errors, isValid } = ValidateUser(req.body)
     try {
-        const data = await Users.findOneAndUpdate( //Prend 3 paramétres
-            {_id:req.params.id},
-            req.body,
-            {new: true}
-        )
-        res.status(201).json(data)
+        if (!isValid) {
+            res.status(404).json(errors);
+        }
+        else {
+            const data = await Users.findOneAndUpdate( //Prend 3 paramétres
+                { _id: req.params.id },
+                req.body,
+                { new: true }
+            )
+            res.status(201).json(data)
+        }
     } catch (error) {
         console.log(error.message)
     }
 }
 
-const deleteUser =  async (req,res)=>{
+const deleteUser = async (req, res) => {
     try {
-        const data = await Users.findOneAndRemove({_id:req.params.id})
-        res.status(201).json({message:"User a été suprimé "})
+        const data = await Users.findOneAndRemove({ _id: req.params.id })
+        res.status(201).json({ message: "User a été suprimé " })
     } catch (error) {
         console.log(error.message)
     }
 }
 
-module.exports={
-    addUser,findAllUsers,findUser,deleteUser,updateUser
+module.exports = {
+    addUser, findAllUsers, findUser, deleteUser, updateUser
 }
--- validation/isEmpty.js
+++ validation/isEmpty.js
@@ -1,6 +1,6 @@
 const isEmpty = (value) =>  //value=req.body.Email
 value === null || value === undefined
 || typeof(value) === "object" && Object.keys(value).length === 0
-|| typeof(value) === "string" && Object.trim().length === 0
+|| typeof(value) === "string" && value.trim().length === 0
 
 module.exports = isEmpty ;
