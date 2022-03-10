760331536da864b2860ac6c3c008576c5fd153b5
FirasKhadraoui <firas.khadraoui@esprit.tn>
2022-03-10 21:50:14 +0100

    FrontCRUD

--- client/src/components/InputGroup.js
+++ client/src/components/InputGroup.js
@@ -1,13 +1,19 @@
 import React from 'react'
+import classnames from 'classnames'
 
-function InputGroup({label, type, name,value}) {
+function InputGroup({ label, type, name, onChangeHandler, errors, value }) {
   return (
     <div className="mb-3">
-    <label for="Email" className="form-label">
-      {label}
-    </label>
-    <input type={type} value={value} name={name}/>
-  </div>
+      <label className="form-label">
+        {label}
+      </label>
+      <input type={type} value={value} className={(classnames("form-control", { "is-invalid": errors }))} name={name} onChange={onChangeHandler} />
+      {
+        errors && (<div class="invalid-feedback">
+          {errors}
+        </div>)
+      }
+    </div>
   )
 }
 
